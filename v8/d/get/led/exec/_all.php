430ff99d26e6b9ee07dd0808b4713960da5cbb05
Sofa eMHa <75196975+sofa-emha@users.noreply.github.com>
2021-02-17 00:23:18 +0700

    Delete v8 directory

--- v8/d/get/led/exec/_all.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$db_get_all = "SELECT * from led"; //? Mendeklarasikan variabel '$db_get_all' dengan value 'SELECT database'
-$req_all = $db->query($db_get_all); //? Membuat variabel baru yang mengeksekusi variabel '$db_get_all'
-while ($row_all = $req_all->fetchArray(SQLITE3_ASSOC)) { //? Fungsi 'while' untuk me-'looping' eksekusi data
-    $exec_all[] = $row_all; //? Menambah array kosong '$exec_all' yang nantinya akan di eksekusi
-}
--- v8/d/get/suhu/exec/_conf.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$get_temp = isset($_GET['temp']) ? $_GET['temp'] : '';
-$get_hum = isset($_GET['hum']) ? $_GET['hum'] : '';
-
-?>
